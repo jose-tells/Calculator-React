801dcf23b1654979855150b59f5bfe60d44a03e3
Jose Marquinez <josemarquinez28@gmail.com>
2022-01-12 18:56:39 -0500

    Fixing bugs

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/dist'),
     filename: 'assets/bundle.js',
     publicPath: '/',
   },
